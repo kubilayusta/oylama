e474777b0e3e63b0a16f6a21b78ae5d6ff217d7b
kubilayusta <149476847+kubilayusta@users.noreply.github.com>
2024-09-04 20:02:04 +0300

    Update config.example.php

--- config.example.php
+++ config.example.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'host' => 'localhost',
-    'username' => 'your_username',
-    'password' => 'your_password',
-    'database' => 'your_database'
+    'username' => 'root',
+    'password' => 'kubilayusta-57',
+    'database' => 'users2'
 ];
